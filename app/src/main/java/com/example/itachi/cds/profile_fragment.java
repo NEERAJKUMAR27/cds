7e5f46bbfe9499ea796a2bc050807ce64802c166
itachi <151210028nk@gmail.com>
2018-03-30 10:44:56 +0530

    commit3

---
+++ app/src/main/java/com/example/itachi/cds/profile_fragment.java
@@ -0,0 +1,38 @@
+package com.example.itachi.cds;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by itachi on 30/3/18.
+ */
+
+public class profile_fragment extends Fragment {
+
+
+    public static profile_fragment  initalizeview(){
+        return new profile_fragment();
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.profile_fragment,container,false);
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+
+}
+
